7a687f19f7d39fccc9ad39943d05287fe2398399
moteesh <moteesh@yahoo.com>
2025-02-06 21:00:52 +0530

    wip

---
+++ myproject/home/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+from datetime import datetime
+
+def home(request):
+    # return HttpResponse("Hello world!!")
+    return render(request, "home/welcome.html", {'today': datetime.today() })
+
+
+@login_required(login_url="/admin")
+def authorized(request):
+    return render(request, "home/authorized.html", {})
