91a74623c62054cc9c23adfe21509fe412bffc33
moteesh <moteesh@yahoo.com>
2025-02-11 21:59:56 +0530

    update

--- my_example_project/notes/urls.py
+++ my_example_project/notes/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("list", views.list)
+    path("list", views.NotesListView.as_view()),
+    path("popular", views.PopularNotesView.as_view()),
+    path("details/<int:pk>", views.NotesDetailView.as_view())
 ]
