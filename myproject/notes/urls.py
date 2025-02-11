7c77903dcead311652c7d783b4a245c28535e6a7
moteesh <moteesh@yahoo.com>
2025-02-11 14:47:52 +0530

    pyc

--- myproject/notes/urls.py
+++ myproject/notes/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("list", views.list),
-    path("details/<int:pk>", views.details),
+    path("list", views.NotesListView.as_view(), name="notes.list"),
+    path("popular", views.PopularNotesListView.as_view()),
+    path("details/<int:pk>", views.NotesDetailView.as_view(), name="notes.detail"),
 ]
--- myproject/notes/views.py
+++ myproject/notes/views.py
@@ -1,17 +1,35 @@
 from django.shortcuts import render
 from django.http import Http404
 
-from django.views.generic import TemplateView
+from django.views.generic import ListView, DetailView
 
 from .models import Notes
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, "notes/notes_list.html", {"notes": all_notes})
 
-def details(request, pk):
-    try:
-        note = Notes.objects.get(pk=pk)
-    except Notes.DoesNotExist:
-        raise Http404("Note doesn't exist")
-    return render(request, "notes/notes_details.html", {"note": note})
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    # template_name = "notes/custom_notes_list.html"
+
+
+# def list(request):
+#     all_notes = Notes.objects.all()
+#     return render(request, "notes/notes_list.html", {"notes": all_notes})
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+    template_name = "notes/notes_details.html"
+
+
+# def details(request, pk):
+#     try:
+#         note = Notes.objects.get(pk=pk)
+#     except Notes.DoesNotExist:
+#         raise Http404("Note doesn't exist")
+#     return render(request, "notes/notes_details.html", {"note": note})
+
+class PopularNotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    queryset = Notes.objects.filter(likes__gte=1)
